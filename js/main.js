d8258d763b3784009368d0be487e4c6c00347c26
Elias Dias Junior <eldias@gmail.com>
2021-03-01 22:23:10 -0300

    Subindo alterações básicas

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+/*
+var count;
+for (count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+
+/* 
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count ++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+
+if (idade >= 18) {
+    alert("Maior de idade.");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {
+                nome:"maçã",
+                cor:"vermelha"
+            };
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+/*
+var nome = "Elias Dias Junior";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(n1 * n2)
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+*/
